0ae1eb2d68781cfc29a6d19666675c1e3f20b3e2
Matías Cruz <matias18233@gmail.com>
2025-04-07 09:23:13 -0300

    Controlador y modelo de tarjeta de crédito

---
+++ pilot-api/app/Http/Controllers/creditCardController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CreditCard;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class creditCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CreditCard $creditCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CreditCard $creditCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CreditCard $creditCard)
+    {
+        //
+    }
+}
