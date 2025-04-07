b1df9663d5395f5856ca47b4a80503e12f73774e
Matías Cruz <matias18233@gmail.com>
2025-04-07 10:20:49 -0300

    Modificaciones sobre los modelos

--- pilot-api/app/Models/CreditCard.php
+++ pilot-api/app/Models/CreditCard.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CreditCard extends Model
 {
-    //
+    protected $table = 'credit_cards';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
 }
--- pilot-api/app/Models/Payment.php
+++ pilot-api/app/Models/Payment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $table = 'payments';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
 }
